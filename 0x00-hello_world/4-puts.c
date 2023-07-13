48e5e5dc79afa9ce2a2460f63d6923039cc9993c
ISMAILTERRAB <terrabismail@gmail.com>
2023-07-13 11:51:34 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
  * main - entry point
- * return: always 0 (success)
- */
+ *
+ * Return: Always 0 (Success)
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
