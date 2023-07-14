8dcf0d84e6d3a83d72a53203217c8dda544af303
ISMAILTERRAB <terrabismail@gmail.com>
2023-07-14 15:20:44 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: all possible combinations of single-digit numbers
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + 0);
+
+		if (digit != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
