0c4c9afa6c2662d799cfbae5c71835c1f5c5e904
ISMAILTERRAB <terrabismail@gmail.com>
2023-07-18 13:07:30 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+"include "main.h"
+
+/**
+ * _isalpha - checks if charcter is a letter lowercase or uppercase
+ *
+ * @c: character in ASCII code
+ *
+ * Return: 1 if 'c' is letter lowercase or uppercase otherwise 0
+*/
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
