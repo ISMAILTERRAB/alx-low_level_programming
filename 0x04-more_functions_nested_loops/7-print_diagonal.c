cae0f584c692e2d8ff37018f6abc3e2213439dd4
ISMAILTERRAB <terrabismail@gmail.com>
2023-07-20 16:29:27 -0700

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -17,9 +17,9 @@ void print_diagonal(int n)
 		for (postn = 1; postn <= n; postn++)
 		{
 			for (space = 1; space <= postn; space++)
-				_puthcar(' ');
+				_putchar(' ');
 			_putchar(92);
-			_puthcar('\n');
+			_putchar('\n');
 		}
 	}
 }
