9a6aa6ff9d8ef095a05fb65da1cc1e490a7e6b76
ISMAILTERRAB <terrabismail@gmail.com>
2023-07-20 16:31:44 -0700

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,7 +3,7 @@
 /**
  * print_diagonal - draws a diagonal line
  *
- * @n: number of time charcter \ should be printed
+ * @n: number of time charcter '\' should be printed
 */
 
 void print_diagonal(int n)
